96a0d297b18f446ba2cc0c05624c80ab0e5a9cae
lebogangdikobo <lebogangdikobo95@gmail.com>
2025-10-27 15:36:00 +0200

    Remove file(s): profile.html

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.tabs button').forEach(btn => {btn.addEventListener('click', e => {
-    document.querySelector('.tabs .active').classList.remove('active');
-    e.target.classList.add('active');
-    });
-});
